7c7a096bdc2ebfa284462ebcc858efc485449729
Shashika Janith Liyanage <shashika.janith.liyanage@gmail.com>
2023-09-21 16:00:49 +0530

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@ class Stack{
     private int[] dataArray;
 
     Stack(){
-        dataArray = new int [0];
+        dataArray = new int [1];
+    }
+    public void push(int data){
+        extendArray();
+        dataArray[dataArray.length-1] = data;
 
     }
+    private void extendArray(){
+        int[] tempDataArray = new int[dataArray.length];
+        for(int i=0; i< tempDataArray.length; i++){
+            tempDataArray[i] = dataArray[i];
+
+        }
+        dataArray = tempDataArray;
+    }
+
 
 }
 
 class Demo {
     public static void main(String[] args) {
     Stack s1 = new Stack();
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(40);
+    s1.push(50);
     }
 }
